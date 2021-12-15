48d1a763f838329c6dca0d2c1a8550a189667827
Kirill Alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2021-12-16 00:10:27 +0300

    fix: send emals

--- client/src/scripts/form/submit.js
+++ client/src/scripts/form/submit.js
@@ -1,10 +1,12 @@
 import { $inputUserName, $inputUserPhone, $btnSendForm } from './validator'
 import { getCorrectCallLink } from '../utils'
 import { getFullDate } from '../utils'
-import { handlerRequest } from '../request'
+// import { handlerRequest } from '../request'
 import { showModalSuccess } from '../UI/modal'
 
-export const submitForm = e => {
+import emailjs from 'emailjs-com'
+
+export const submitForm = async e => {
 	e.preventDefault()
 
 	if ($btnSendForm.classList.contains('_disable')) return
@@ -12,22 +14,26 @@ export const submitForm = e => {
 	const { value: name } = $inputUserName
 	const { value: phone } = $inputUserPhone
 
-	const userData = {
+	const sendData = {
 		callLink: getCorrectCallLink(phone),
 		date: getFullDate(),
 		name,
 		phone,
 	}
 
-	handlerRequest('/', 'POST', userData).then(data => {
-		const { name, status } = data
+	await emailjs.send('service_solomka_id', 'solomka_email_id', sendData, 'user_CQyKldP8r2sZRGhvJbXfs')
+	await showModalSuccess(name)
+	await clearFomr()
 
-		if (status === 200) {
-			showModalSuccess(name)
-			clearFomr()
-			console.log(data)
-		}
-	})
+	// handlerRequest('/', 'POST', userData).then(data => {
+	// 	const { name, status } = data
+
+	// 	if (status === 200) {
+		// showModalSuccess(name)
+		// clearFomr()
+	// console.log(data)
+	// 	}
+	// })
 }
 
 const clearFomr = () => {
